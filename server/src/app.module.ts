f88e5fc2d768089bca5a1986f8828192ebda68bd
Ygor <ygor.araujo@dataprev.gov.br>
2025-03-08 11:26:25 -0300

    imported libs & db configs

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './modules/user/user.entity'; 
+import { AuthModule } from './modules/auth/auth.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: './data/db.sqlite',
+      entities: [User],
+      synchronize: true,
+    }),
+    MongooseModule.forRoot('mongodb://localhost:27017/creatorsfy'),
+    // AuthModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
